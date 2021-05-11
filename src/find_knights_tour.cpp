69048d30e10a259211e00dce661e001a23c6ce20
Charlotte Lastname <lancharlotl@gmail.com>
2021-05-11 19:39:50 -0400

    Better comments clarifying the algorithm.

--- src/find_knights_tour.cpp
+++ src/find_knights_tour.cpp
@@ -24,8 +24,10 @@ void find_knights_tour(Board& b, int sr, int sc) {
 			success = false;
 			break;
 		}
+		// if the algorithm backtracks from the very first node, it has exhausted the tree
 		
 		SmartTilePointer& t = route.back();
+		// for brevity, more than anything
 		
 		if (t->direction >= t->total_neighbors) {
 			b.remove_tile_from_route(route);
@@ -33,14 +35,18 @@ void find_knights_tour(Board& b, int sr, int sc) {
 				route.back()->increment_direction();
 			continue;
 		}
+		// if we've exhausted all possible directions from a node, the node itself is exhausted
 		
 		if (t->neighbors[t->direction]->used) {
 			t->increment_direction(); continue;
 		}
+		// if a neighbor is already being used by the path, we can't use it again
 		
+		// if none of the above occurs, we can add a new node to the path!
 		SmartTilePointer& new_tile = t->neighbors[t->direction];
 		b.add_tile_to_route(new_tile, route);
 		
+		// but if adding this node causes a dead-end board-state, we consider the new node exhausted and backtrack
 		if (b.unsolvable(new_tile)) {
 			b.remove_tile_from_route(route);
 			route.back()->increment_direction();
@@ -61,6 +67,5 @@ void find_knights_tour(Board& b, int sr, int sc) {
 		std::cout << "Couldn't find a knight's tour!\n";
 		
 	}
-	
 	
 }
